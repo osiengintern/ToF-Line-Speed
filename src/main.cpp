764c8b5fa677d7838a87c9700533869a1c1c8c65
osiengintern <engintern@ohiosteel.com>
2023-07-07 11:25:49 -0400

    Wrapper Class Phase 1 (Still WIP)

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,30 @@
 // DS3231 Real Time Clock stuff
 RTC_DS3231 rtc;
 
-// VL53L1X Time of Flight stuff
-Adafruit_VL53L1X vl53 = Adafruit_VL53L1X();
-
 // 1.2" 7-Segment Display stuff
-Adafruit_7segment display1 = Adafruit_7segment();
-Adafruit_7segment display2 = Adafruit_7segment();
+Adafruit_7segment FPSDisplay = Adafruit_7segment();
+Adafruit_7segment clockDisplay = Adafruit_7segment();
+
+// VL53L1X Class Definition
+class ToFSensor {
+  public:
+    ToFSensor();
+    int getDistance();
+  private:
+    Adafruit_VL53L1X VL53Instance;
+    unsigned long millisObjectDetected;
+    unsigned long millisObjectLost;
+};
+
+ToFSensor::ToFSensor() {
+  VL53Instance = Adafruit_VL53L1X();
+  millisObjectDetected, millisObjectLost = 0;
+}
+
+int ToFSensor::getDistance() {
+  return VL53Instance.distance();
+}
+
 
 // counters and other relevant variables
 int vl53_distance;
@@ -22,12 +40,33 @@ boolean isObjectDetected = false;
 boolean pastObjectDetect = false;
 
 unsigned long timeWhenTOFLost = 0, timeWhenTOFRestored, timeTOFDifference;
-boolean wasTOFLost = false;
+boolean isVL53Lost = false;
+
+
+unsigned long previousMillis_RTCPrintOut = 0; // the last time the time was printed to serial from the RTC
+long RTCPrintInterval = 1000; // the time (in milliseconds) in between RTC print outs
+
+// function prototypes
+void VL53IsLost();
+void printCurrentTime(DateTime);
 
 void setup() {
-  while (!Serial) {}
   Serial.begin(115200);
   Wire.begin();
+
+  // VL53L1X Time of Flight stuff
+  ToFSensor ToFOne, ToFTwo;
+  int distanceOne = ToFOne.getDistance();
+
+  #ifndef ESP8266
+    while (!Serial); // wait for serial port to connect. Needed for native USB
+  #endif
+
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    Serial.flush();
+    while (1) delay(10);
+  }
   
   if (! vl53.begin(0x29, &Wire)) {
     Serial.print(F("Error on init of VL sensor: "));
@@ -35,10 +74,8 @@ void setup() {
     while (1)       delay(10);
   }
   Serial.println(F("VL53L1X sensor OK!"));
-
   Serial.print(F("Sensor ID: 0x"));
   Serial.println(vl53.sensorID(), HEX);
-
   if (! vl53.startRanging()) {
     Serial.print(F("Couldn't start ranging: "));
     Serial.println(vl53.vl_status);
@@ -47,25 +84,40 @@ void setup() {
   Serial.println(F("Ranging started"));
 
   // Valid timing budgets: 15, 20, 33, 50, 100, 200 and 500ms!
-  vl53.setTimingBudget(200);
+  vl53.setTimingBudget(15);
   Serial.print(F("Timing budget (ms): "));
   Serial.println(vl53.getTimingBudget());
   
-  vl53.VL53L1X_SetDistanceMode(1);
+  //vl53.VL53L1X_SetDistanceMode(1); i dont think this does anything
 
   // 1.2" 7-Segment Display stuff
-  display1.begin(0x71);
-  display2.begin(0x70);
+  FPSDisplay.begin(0x71);
+  clockDisplay.begin(0x70);
 }
 
 void loop() {
+  DateTime now = rtc.now();
+  printCurrentTime(now);
 
   if (vl53.dataReady()) {
 
     // new measurement for the taking!
     vl53_distance = vl53.distance();
 
-    if (vl53_distance == -1) {
+    if (vl53_distance)
+
+
+
+    if (vl53_distance == -1) { // the sensor is no longer able to detect something
+      if (!isVL53Lost) { // only runs once (the first time when measurements are lost)
+        VL53IsLost();
+      }
+      return;
+    } else {
+      isVL53Lost = false;
+    }
+
+    /*if (vl53_distance == -1) {
       // something went wrong! (lost object)
 
       if (!wasTOFLost) {
@@ -74,8 +126,8 @@ void loop() {
         pastObjectDetect = false;
       }
 
-      display1.print(99999, DEC);
-      display1.writeDisplay();
+      FPSDisplay.print(99999, DEC);
+      FPSDisplay.writeDisplay();
 
       Serial.print(F("VL53L1X couldn't get distance! -> "));
       Serial.println(vl53.vl_status);
@@ -91,7 +143,7 @@ void loop() {
       } else {
         wasTOFLost = false;
       }
-    }
+    }*/
 
     if (vl53_distance <= 800) {
       isObjectDetected = true;
@@ -106,25 +158,53 @@ void loop() {
       pastObjectDetect = false;
     }
 
-    Serial.println(isObjectDetected);
-    Serial.println(pastObjectDetect);
-    Serial.println();
-
-    Serial.println(F("Distances (mm): "));
+    Serial.print(F("Distances (mm): "));
     Serial.println(vl53_distance);
+    Serial.println();
 
     // data is read out, time for another reading!
     vl53.clearInterrupt();
 
   }
 
-  display1.print(vl53_distance);
-  display1.writeDisplay();
+  FPSDisplay.print(vl53_distance);
+  FPSDisplay.writeDisplay();
 
-  display2.print(transitCounter);
-  display2.writeDisplay();
+  clockDisplay.print(transitCounter);
+  clockDisplay.writeDisplay();
 
 
-  delay(100);
+  //delay(200);
 
+}
+
+void VL53IsLost() {
+  timeWhenTOFLost = millis();
+
+
+  Serial.print(F("VL53L1X couldn't get distance!"));
+  isVL53Lost = true;
+}
+
+void printCurrentTime(DateTime now) {
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis_RTCPrintOut >= RTCPrintInterval) {
+    Serial.println();
+    Serial.print(F("Current Time from DS3231 RTC: "));
+    Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print(F(" "));
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println();
+
+    previousMillis_RTCPrintOut = millis();
+  }  
 }
