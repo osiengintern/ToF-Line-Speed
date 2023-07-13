0259cc67746a319057339aa774d6b00fe6fb9b39
osiengintern <engintern@ohiosteel.com>
2023-07-13 13:28:48 -0400

    Skipped Sensor Validation Added

--- src/main.cpp
+++ src/main.cpp
@@ -9,27 +9,31 @@
 #include "RTClib.h"
 
 #define REFLECTOR_DISTANCE 1000 //millimeters
-#define RTC_PRINT_INTERVAL 1000 //milliseconds (the time in between RTC print outs)
+#define RTC_PRINT_INTERVAL 5 //seconds (the time in between RTC print outs)
 #define OBJECT_DETECTION_THRESHOLD 200 //milliseconds
-#define ROLLING_AVERAGE_ARRAY_SIZE 2
+#define ROLLING_AVERAGE_ARRAY_SIZE 3
 #define INCHES_BETWEEN_VL53L1X_SENSORS 4
 
 // DS3231 Real Time Clock stuff
 RTC_DS3231 rtc;
+unsigned long previousMillis_RTCPrintOut = 0; // the last time the time was printed to serial from the RTC
 
 // 1.2" 7-Segment Display stuff
 Adafruit_7segment FPSDisplay = Adafruit_7segment();
 Adafruit_7segment clockDisplay = Adafruit_7segment();
 
-// function prototypes
 void printCurrentTime(DateTime);
 void printDebugToSerial();
-int addTimeToRollingArray(unsigned long[], unsigned long);
-void calculateLineSpeedAndAddToArray(int);
+int findNextAvailableIndexInArray(unsigned long[]);
+int addValueToArray(unsigned long[], unsigned long, int);
+int addValueToArray(double[], double, int);
+void calculateLineSpeed(int);
 
 unsigned long VLOneDetectionTimes[ROLLING_AVERAGE_ARRAY_SIZE]; // 0x2A (42)
 unsigned long VLTwoDetectionTimes[ROLLING_AVERAGE_ARRAY_SIZE]; // 0x2B (43)
 double lineSpeeds[ROLLING_AVERAGE_ARRAY_SIZE];
+
+int VLOneTransitCounter = 0, VLTwoTransitCounter = 0;
 
 // THE AMAZING VARIABLE CONTAINING WHAT WE CARE ABOUT MOST
 double rollingAverageLineSpeed = 0;
@@ -40,9 +44,8 @@ class ToFSensor : public Adafruit_VL53L1X {
     ToFSensor(int);
     int I2C_ID;
     void takeMeasurement() { measurementDistance = distance(); };
-    void objectDetectCheck(ToFSensor[]);
+    void objectDetectCheck();
     int measurementDistance;
-    int transitCounter;
     unsigned long millisObjectDetected, millisObjectLost, holdingMillis; 
     boolean previousObjectDetected, previousMeasurementLost;    
 };
@@ -51,10 +54,10 @@ class ToFSensor : public Adafruit_VL53L1X {
   // this variable will hold the time that an object was detected until 200 milliseconds have passed, after which it will pass the value on to millisObjectDetected.
   // this ensures that whenever millisObjectDetected is pulled, it is the most recent time that a confirmed object has passed the sensor.
 ToFSensor::ToFSensor(int ID) {
-  I2C_ID = ID, millisObjectDetected = 0, millisObjectLost = 0, holdingMillis = 0, transitCounter = 0;
+  I2C_ID = ID, millisObjectDetected = 0, millisObjectLost = 0, holdingMillis = 0;
 }
 
-void ToFSensor::objectDetectCheck(ToFSensor sensorArray[]) {
+void ToFSensor::objectDetectCheck() {
 // TODO: might want to add a minimum distance range as well.
   if (measurementDistance == -1 || measurementDistance >= REFLECTOR_DISTANCE) { // the sensor is no longer able to detect a T-Bar/Component
     if (!previousMeasurementLost) { // only runs once (the first time when sensor can no longer detect a T-Bar)
@@ -69,6 +72,7 @@ void ToFSensor::objectDetectCheck(ToFSensor sensorArray[]) {
     if (millis() - millisObjectLost > OBJECT_DETECTION_THRESHOLD) { // if the sensor doesn't see a t-bar for more than 0.2 seconds, consider the component has gone past the sensor
       previousObjectDetected = false;
     }
+
   } else { // the sensor CAN see a t-bar/component    
 
     if (previousMeasurementLost) { // the first time the sensor does detect a t-bar
@@ -84,47 +88,83 @@ void ToFSensor::objectDetectCheck(ToFSensor sensorArray[]) {
       millisObjectDetected = holdingMillis;
       previousObjectDetected = true;
 
+      int nextVLOneIndex = findNextAvailableIndexInArray(VLOneDetectionTimes);
+      int nextVLTwoIndex = findNextAvailableIndexInArray(VLTwoDetectionTimes);
+
       if (I2C_ID == 0x2A) { // components pass by this sensor first
-        //TODO: this needs to be done more intelligently.
-        int nextEmptyIndex = addTimeToRollingArray(VLOneDetectionTimes, millisObjectDetected);
 
-        if (transitCounter > sensorArray[1].transitCounter) { // covers the case where the second sensor missed a component
-          VLOneDetectionTimes[nextEmptyIndex] = 0; // reset the most recent addition to the second data array
+        // redundancy case if the second sensor misses a component.
+        if (VLOneTransitCounter > VLTwoTransitCounter) {
 
-          //TODO: please fix this! we only want throw out one bad data point, not two.
-          VLOneDetectionTimes[nextEmptyIndex - 1] = 0; // reset the second most recent addition to the second data array as well. TODO: fix maybe?
-          sensorArray[0].transitCounter--; // because we cut out two data points, we have to reduce its transit counter by 1. TODO: this is really unoptimal.
+          if (nextVLTwoIndex == -1) {
+            addValueToArray(VLOneDetectionTimes, millisObjectDetected, ROLLING_AVERAGE_ARRAY_SIZE - 1);
+          } else {
+            // insert the value at the latest matching index, and replace the current value.
+            addValueToArray(VLOneDetectionTimes, millisObjectDetected, nextVLTwoIndex);
+          }
 
           Serial.print(F("VL53L1X ("));
           Serial.print(I2C_ID);
           Serial.println(F("): Data discarded! (Sensor 43 missed data.)"));
-          return;
+
+        } else { // what we expect to happen
+          addValueToArray(VLOneDetectionTimes, millisObjectDetected, nextVLOneIndex);
+
+          VLOneTransitCounter++;
+
+          Serial.print(F("VL53L1X ("));
+          Serial.print(I2C_ID);
+          Serial.println(F("): Object successfully detected!"));
         }
 
       } else if (I2C_ID == 0x2B) { // components pass by this sensor second
-        //TODO: this needs to be done more intelligently.
-        int nextEmptyIndex = addTimeToRollingArray(VLTwoDetectionTimes, millisObjectDetected);
 
-        if (transitCounter >= sensorArray[0].transitCounter) { // covers the case where the first sensor missed a component
-          VLTwoDetectionTimes[nextEmptyIndex] = 0; // reset the most recent addition to the second data array
+        // redundancy case if the first sensor misses a component.
+        if (VLTwoTransitCounter == VLOneTransitCounter) {
 
           Serial.print(F("VL53L1X ("));
           Serial.print(I2C_ID);
           Serial.println(F("): Data discarded! (Sensor 42 missed data.)"));
-          return;
-        } else {
-          calculateLineSpeedAndAddToArray(nextEmptyIndex);
+
+        } else { // what we expect to happen
+          int finalIndexLocation = addValueToArray(VLTwoDetectionTimes, millisObjectDetected, nextVLTwoIndex);
+          calculateLineSpeed(finalIndexLocation);
+
+          VLTwoTransitCounter++;
+
+          Serial.print(F("VL53L1X ("));
+          Serial.print(I2C_ID);
+          Serial.println(F("): Object successfully detected!"));
         }
-        
+
       }
 
-      transitCounter++; // maybe here at the end? where it is confirmed that a part has passed by both components
+      Serial.print(F("nextVLOneIndex: "));
+      Serial.println(nextVLOneIndex);
+      Serial.print(F("nextVLTwoIndex: "));
+      Serial.println(nextVLTwoIndex);
 
-      Serial.print(F("VL53L1X ("));
-      Serial.print(I2C_ID);
-      Serial.print(F("): Object successfully detected! (Transit Counter: "));
-      Serial.print(transitCounter);
-      Serial.println(F(")"));
+      Serial.print(F("oneTransit: "));
+      Serial.println(VLOneTransitCounter);
+      Serial.print(F("twoTransit: "));
+      Serial.println(VLTwoTransitCounter);
+
+      Serial.println();
+      for (int i = 0; i < ROLLING_AVERAGE_ARRAY_SIZE; i++) {
+        Serial.print(VLOneDetectionTimes[i]);
+        Serial.print(F(" "));
+      }
+      Serial.println();
+      for (int i = 0; i < ROLLING_AVERAGE_ARRAY_SIZE; i++) {
+        Serial.print(VLTwoDetectionTimes[i]);
+        Serial.print(F(" "));
+      }
+      Serial.println();
+      for (int i = 0; i < ROLLING_AVERAGE_ARRAY_SIZE; i++) {
+        Serial.print(lineSpeeds[i]);
+        Serial.print(F(" "));
+      }
+      Serial.println();
     }
   }
 }
@@ -133,7 +173,7 @@ void ToFSensor::objectDetectCheck(ToFSensor sensorArray[]) {
 ToFSensor VLSensorArray[] = { ToFSensor(0x2A), ToFSensor(0x2B) }; // IDs are hard coded simply because there are only two VL53L1X sensors being used.
 const int xshutPins[2] = {5, 6};
 
-unsigned long previousMillis_RTCPrintOut = 0; // the last time the time was printed to serial from the RTC
+// Counter that tracks the hertz rate of the program.
 int DEBUG_HERTZ_COUNTER;
 
 void setup() {
@@ -209,19 +249,19 @@ void setup() {
 
 void loop() {
   DateTime now = rtc.now();
-  //  printCurrentTime(now);
+  //printCurrentTime(now);
 
   if (VLSensorArray[0].dataReady()) {
 
     VLSensorArray[0].takeMeasurement();
-    VLSensorArray[0].objectDetectCheck(VLSensorArray);
+    VLSensorArray[0].objectDetectCheck();
 
   }
 
   if (VLSensorArray[1].dataReady()) {
 
     VLSensorArray[1].takeMeasurement();
-    VLSensorArray[1].objectDetectCheck(VLSensorArray);
+    VLSensorArray[1].objectDetectCheck();
 
   }  
 
@@ -238,7 +278,10 @@ void loop() {
 
 }
 
-int addTimeToRollingArray(unsigned long array[], unsigned long valueToAdd) {
+// MAIN FUNCTIONS
+
+// Returns the next available index in an array, and -1 if the array is considered full and needs to be shifted to the left by one element.
+int findNextAvailableIndexInArray(unsigned long array[]) {
   int nextAvailableIndex = -1;
 
   for (int i = 0; i < ROLLING_AVERAGE_ARRAY_SIZE; i++) {
@@ -247,25 +290,63 @@ int addTimeToRollingArray(unsigned long array[], unsigned long valueToAdd) {
       break;
     }
   }
+  
+  return nextAvailableIndex;
+}
 
-  if (nextAvailableIndex == -1) { // no available slots in the rolling average array
-    for (int i = 0; i < ROLLING_AVERAGE_ARRAY_SIZE - 1; i++) {
+// Adds an unsigned long to an unsigned long array into a specific index.
+// If the 'index' input is -1, the array is considered full and is shifted down, creating a rolling array.
+// Returns the index that the valueToInsert was ultimately placed in.
+int addValueToArray(unsigned long array[], unsigned long valueToInsert, int index) {
+
+  // If the index is -1, this means that the array is full with data values, and must be shifted down.
+  if (index == -1) { 
+
+    // shift all values in the array one element to the left to make room for the new value.
+    for (int i = 0; i < ROLLING_AVERAGE_ARRAY_SIZE - 1; i++) { 
       array[i] = array[i+1];
     }
 
-    nextAvailableIndex = ROLLING_AVERAGE_ARRAY_SIZE - 1;
+    // this sets the index variable (which lets the function know what index to add the valueToInsert variable to) to the maximum possible index, which
+    // should be empty.
+    index = ROLLING_AVERAGE_ARRAY_SIZE - 1;
   }
 
-  array[nextAvailableIndex] = valueToAdd;
-
-  return nextAvailableIndex; // returns what index the latest object detection time was placed in
+  // adds the valueToInsert into the array at the appropriate location.
+  array[index] = valueToInsert;
+  
+  return index;
 }
 
-void calculateLineSpeedAndAddToArray(int latestIndexPlacement) { // takes in where the latest object detection time was placed in
+// Adds an unsigned long to an unsigned long array into a specific index.
+// If the 'index' input is -1, the array is considered full and is shifted down, creating a rolling array.
+// Returns the index that the valueToInsert was ultimately placed in.
+int addValueToArray(double array[], double valueToInsert, int index) {
+
+  // If the index is -1, this means that the array is full with data values, and must be shifted down.
+  if (index == -1) { 
+
+    // shift all values in the array one element to the left to make room for the new value.
+    for (int i = 0; i < ROLLING_AVERAGE_ARRAY_SIZE - 1; i++) { 
+      array[i] = array[i+1];
+    }
+
+    // this sets the index variable (which lets the function know what index to add the valueToInsert variable to) to the maximum possible index, which
+    // should be empty.
+    index = ROLLING_AVERAGE_ARRAY_SIZE - 1;
+  }
+
+  // adds the valueToInsert into the array at the appropriate location.
+  array[index] = valueToInsert;
+
+  return index;
+}
+
+void calculateLineSpeed(int index) {
   double distanceBetweenSensorsInFeet = INCHES_BETWEEN_VL53L1X_SENSORS / 12.0;
 
-  unsigned long timeOne = VLOneDetectionTimes[latestIndexPlacement]; // this is first as it should be. TODO: clean up.
-  unsigned long timeTwo = VLTwoDetectionTimes[latestIndexPlacement]; 
+  unsigned long timeOne = VLOneDetectionTimes[index];
+  unsigned long timeTwo = VLTwoDetectionTimes[index]; 
 
   unsigned long timeDifInMS = timeTwo - timeOne;
 
@@ -273,36 +354,17 @@ void calculateLineSpeedAndAddToArray(int latestIndexPlacement) { // takes in whe
 
   double lineSpeedInFPM = distanceBetweenSensorsInFeet / timeDifInMinutes;
 
-  int nextAvailableIndex = -1;
-
-
-  // adds it to the rolling array
-  for (int i = 0; i < ROLLING_AVERAGE_ARRAY_SIZE; i++) {
-    if (lineSpeeds[i] == 0) {
-      nextAvailableIndex = i;
-      break;
-    }
-  }
-
-  if (nextAvailableIndex == -1) { // no available slots in the rolling average array
-    for (int i = 0; i < ROLLING_AVERAGE_ARRAY_SIZE - 1; i++) {
-      lineSpeeds[i] = lineSpeeds[i+1];
-    }
-
-    nextAvailableIndex = ROLLING_AVERAGE_ARRAY_SIZE - 1;
-  }
-
-  lineSpeeds[nextAvailableIndex] = lineSpeedInFPM;
-
+  addValueToArray(lineSpeeds, lineSpeedInFPM, index);
 
   // calculates the rolling average. nextAvailableIndex + 1 tells you how many values are in the array at the time, used to calculate the denominator of the average
   double sum = 0.0;
 
-  for (int i = 0; i <= nextAvailableIndex; i++) {
+  for (int i = 0; i <= index; i++) {
     sum += lineSpeeds[i];
   }
 
-  rollingAverageLineSpeed = sum / (nextAvailableIndex + 1);
+  //TODO: see if this still works with the data validation system.
+  rollingAverageLineSpeed = sum / (index + 1);
 
   Serial.print(F("linespeed: "));
   for (int i = 0; i < ROLLING_AVERAGE_ARRAY_SIZE; i++) {
@@ -313,8 +375,8 @@ void calculateLineSpeedAndAddToArray(int latestIndexPlacement) { // takes in whe
 
   Serial.print(F("rolling avr: "));
   Serial.println(rollingAverageLineSpeed);
-  Serial.print(F("nextAvailable: "));
-  Serial.println(nextAvailableIndex);
+  Serial.print(F("index: "));
+  Serial.println(index);
   Serial.print(F("sum: "));
   Serial.println(sum);
 }
@@ -361,7 +423,7 @@ void printDebugToSerial() {
 void printCurrentTime(DateTime now) {
   unsigned long currentMillis = millis();
 
-  if (currentMillis - previousMillis_RTCPrintOut >= RTC_PRINT_INTERVAL) {
+  if (currentMillis - previousMillis_RTCPrintOut >= (RTC_PRINT_INTERVAL * 1000)) {
     Serial.println();
     Serial.print(F("Current Time from DS3231 RTC: "));
     Serial.print(now.year(), DEC);
@@ -378,7 +440,7 @@ void printCurrentTime(DateTime now) {
     Serial.println();
 
     Serial.print(F("The program is running at "));
-    Serial.print(DEBUG_HERTZ_COUNTER);
+    Serial.print(DEBUG_HERTZ_COUNTER / RTC_PRINT_INTERVAL);
     Serial.println(F(" Hertz."));
     Serial.println();
 
